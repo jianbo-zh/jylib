ec2080a6d2e5d96d26e6b91d1d4013dabfb50a39
jianbo-zh <tipine@163.com>
2025-05-20 17:12:14 +0800

    debug

--- go.mod
+++ go.mod
@@ -3,15 +3,17 @@ module github.com/jianbo-zh/jylib
 go 1.23.5
 
 require (
+	github.com/go-kratos/kratos/contrib/registry/etcd/v2 v2.0.0-20250429074618-c82f7957223f
 	github.com/go-kratos/kratos/v2 v2.8.4
 	github.com/golang-jwt/jwt/v5 v5.1.0
 	github.com/jftuga/geodist v1.0.0
-	github.com/jianbo-zh/jyapi v0.1.2
+	github.com/jianbo-zh/jyapi v0.1.19
 	github.com/qichengzx/coordtransform v0.0.0-20220708113031-010878418826
 	github.com/quic-go/quic-go v0.51.0
 	github.com/spf13/cast v1.8.0
 	github.com/wechatpay-apiv3/wechatpay-go v0.2.20
 	github.com/wenzhenxi/gorsa v0.0.0-20230530123828-0320cce15d81
+	go.etcd.io/etcd/client/v3 v3.5.11
 	go.opentelemetry.io/otel v1.35.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.35.0
 	go.opentelemetry.io/otel/exporters/prometheus v0.57.0
@@ -26,12 +28,20 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/go-kratos/aegis v0.2.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
-	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/gnostic v0.7.0 // indirect
+	github.com/google/gnostic-models v0.6.9-0.20230804172637-c7be7c783f49 // indirect
+	github.com/google/pprof v0.0.0-20210720184732-4bb14d4b1be1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.1 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/onsi/ginkgo/v2 v2.9.5 // indirect
@@ -39,11 +49,16 @@ require (
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.11 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.11 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.35.0 // indirect
 	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/mock v0.5.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/mod v0.18.0 // indirect
 	golang.org/x/net v0.37.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
---
+++ grpcc/grpcc.go
@@ -0,0 +1,253 @@
+package grpcc
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-kratos/kratos/contrib/registry/etcd/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/jianbo-zh/jylib/helpc"
+	"github.com/jianbo-zh/jylib/typec"
+	"github.com/jianbo-zh/jylib/util/tlsconf"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"google.golang.org/protobuf/types/known/durationpb"
+
+	authV1 "github.com/jianbo-zh/jyapi/api/carauth/v1"
+	configV1 "github.com/jianbo-zh/jyapi/api/carconfig/v1"
+	dispatchV1 "github.com/jianbo-zh/jyapi/api/cardispatch/v1"
+	orderV1 "github.com/jianbo-zh/jyapi/api/carorder/v1"
+	proxyV1 "github.com/jianbo-zh/jyapi/api/carproxy/v1"
+	filestorageV1 "github.com/jianbo-zh/jyapi/api/filestorage/v1"
+	messageV1 "github.com/jianbo-zh/jyapi/api/message/v1"
+	parkmapV1 "github.com/jianbo-zh/jyapi/api/parkmap/v1"
+)
+
+type IClient interface {
+	CarAuthClient(context.Context) (authV1.CarAuthClient, error)
+	CarConfigClient(context.Context) (configV1.ConfigClient, error)
+	CarDispatchClient(context.Context) (dispatchV1.DispatchClient, error)
+	CarOrderClient(context.Context) (orderV1.CarOrderClient, error)
+	CarProxyClient(context.Context) (proxyV1.CarProxyClient, error)
+	FileStorageClient(context.Context) (filestorageV1.FileStorageClient, error)
+	MessageClient(context.Context) (messageV1.MessageClient, error)
+	ParkMapClient(context.Context) (parkmapV1.ParkMapClient, error)
+}
+
+type Client struct {
+	env       string
+	discovery *etcd.Registry
+	logger    log.Logger
+}
+
+type TlsConf struct {
+	CaCrt  string
+	SvrKey string
+	SvrCrt string
+}
+
+type EtcdConf struct {
+	Username    string
+	Password    string
+	Endpoints   []string
+	TlsConf     *TlsConf
+	DialTimeout *durationpb.Duration
+}
+
+func NewClient(env string, etcdConf *EtcdConf) IClient {
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints:   etcdConf.Endpoints,
+		Username:    etcdConf.Username,
+		Password:    etcdConf.Password,
+		TLS:         tlsconf.NewServerTlsConfig(etcdConf.TlsConf.SvrKey, etcdConf.TlsConf.SvrCrt, etcdConf.TlsConf.CaCrt),
+		DialTimeout: etcdConf.DialTimeout.AsDuration(),
+	})
+	if err != nil {
+		panic(err)
+	}
+	discovery := etcd.New(client)
+
+	return &Client{
+		env:       env,
+		discovery: discovery,
+	}
+}
+
+// CarAuthClient
+func (c *Client) CarAuthClient(ctx context.Context) (authV1.CarAuthClient, error) {
+	conn, err := grpc.Dial(ctx,
+		grpc.WithDiscovery(c.discovery),
+		grpc.WithEndpoint(helpc.ServerEndpoint(c.env, typec.Service_CarAuth)),
+		grpc.WithTimeout(5*time.Second),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+			tracing.Client(),
+		),
+		grpc.WithLogger(c.logger),
+		grpc.WithTLSConfig(
+			tlsconf.NewClientTlsConfig("", "", ""),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("grpc.Dial error: %w", err)
+	}
+
+	return authV1.NewCarAuthClient(conn), nil
+}
+
+// CarConfigClient
+func (c *Client) CarConfigClient(ctx context.Context) (configV1.ConfigClient, error) {
+	conn, err := grpc.Dial(ctx,
+		grpc.WithDiscovery(c.discovery),
+		grpc.WithEndpoint(helpc.ServerEndpoint(c.env, typec.Service_Config)),
+		grpc.WithTimeout(5*time.Second),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+			tracing.Client(),
+		),
+		grpc.WithLogger(c.logger),
+		grpc.WithTLSConfig(
+			tlsconf.NewClientTlsConfig("", "", ""),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("grpc.Dial error: %w", err)
+	}
+
+	return configV1.NewConfigClient(conn), nil
+}
+
+// CarDispatchClient
+func (c *Client) CarDispatchClient(ctx context.Context) (dispatchV1.DispatchClient, error) {
+	conn, err := grpc.Dial(ctx,
+		grpc.WithDiscovery(c.discovery),
+		grpc.WithEndpoint(helpc.ServerEndpoint(c.env, typec.Service_Dispatch)),
+		grpc.WithTimeout(5*time.Second),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+			tracing.Client(),
+		),
+		grpc.WithLogger(c.logger),
+		grpc.WithTLSConfig(
+			tlsconf.NewClientTlsConfig("", "", ""),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("grpc.Dial error: %w", err)
+	}
+
+	return dispatchV1.NewDispatchClient(conn), nil
+}
+
+// CarOrderClient
+func (c *Client) CarOrderClient(ctx context.Context) (orderV1.CarOrderClient, error) {
+	conn, err := grpc.Dial(ctx,
+		grpc.WithDiscovery(c.discovery),
+		grpc.WithEndpoint(helpc.ServerEndpoint(c.env, typec.Service_CarOrder)),
+		grpc.WithTimeout(5*time.Second),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+			tracing.Client(),
+		),
+		grpc.WithLogger(c.logger),
+		grpc.WithTLSConfig(
+			tlsconf.NewClientTlsConfig("", "", ""),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("grpc.Dial error: %w", err)
+	}
+
+	return orderV1.NewCarOrderClient(conn), nil
+}
+
+// CarProxyClient
+func (c *Client) CarProxyClient(ctx context.Context) (proxyV1.CarProxyClient, error) {
+	conn, err := grpc.Dial(ctx,
+		grpc.WithDiscovery(c.discovery),
+		grpc.WithEndpoint(helpc.ServerEndpoint(c.env, typec.Service_CarProxy)),
+		grpc.WithTimeout(5*time.Second),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+			tracing.Client(),
+		),
+		grpc.WithLogger(c.logger),
+		grpc.WithTLSConfig(
+			tlsconf.NewClientTlsConfig("", "", ""),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("grpc.Dial error: %w", err)
+	}
+
+	return proxyV1.NewCarProxyClient(conn), nil
+}
+
+// FileStorageClient
+func (c *Client) FileStorageClient(ctx context.Context) (filestorageV1.FileStorageClient, error) {
+	conn, err := grpc.Dial(ctx,
+		grpc.WithDiscovery(c.discovery),
+		grpc.WithEndpoint(helpc.ServerEndpoint(c.env, typec.Service_FileStorage)),
+		grpc.WithTimeout(5*time.Second),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+			tracing.Client(),
+		),
+		grpc.WithLogger(c.logger),
+		grpc.WithTLSConfig(
+			tlsconf.NewClientTlsConfig("", "", ""),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("grpc.Dial error: %w", err)
+	}
+
+	return filestorageV1.NewFileStorageClient(conn), nil
+}
+
+// MessageClient
+func (c *Client) MessageClient(ctx context.Context) (messageV1.MessageClient, error) {
+	conn, err := grpc.Dial(ctx,
+		grpc.WithDiscovery(c.discovery),
+		grpc.WithEndpoint(helpc.ServerEndpoint(c.env, typec.Service_Message)),
+		grpc.WithTimeout(5*time.Second),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+			tracing.Client(),
+		),
+		grpc.WithLogger(c.logger),
+		grpc.WithTLSConfig(
+			tlsconf.NewClientTlsConfig("", "", ""),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("grpc.Dial error: %w", err)
+	}
+
+	return messageV1.NewMessageClient(conn), nil
+}
+
+// ParkMapClient
+func (c *Client) ParkMapClient(ctx context.Context) (parkmapV1.ParkMapClient, error) {
+	conn, err := grpc.Dial(ctx,
+		grpc.WithDiscovery(c.discovery),
+		grpc.WithEndpoint(helpc.ServerEndpoint(c.env, typec.Service_ParkMap)),
+		grpc.WithTimeout(5*time.Second),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+			tracing.Client(),
+		),
+		grpc.WithLogger(c.logger),
+		grpc.WithTLSConfig(
+			tlsconf.NewClientTlsConfig("", "", ""),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("grpc.Dial error: %w", err)
+	}
+
+	return parkmapV1.NewParkMapClient(conn), nil
+}
