0dac1aafb6632b6d505b45a1bb5e820301194543
jianbo-zh <tipine@163.com>
2025-05-21 21:37:55 +0800

    debug

--- bootstrap/flag.go
+++ bootstrap/flag.go
@@ -3,24 +3,27 @@ package bootstrap
 import "flag"
 
 type CommandFlags struct {
-	Conf       string
 	Env        string
-	ConfigHost string
+	ConfigPath string
 	ConfigType string
+	ConfigHost string
+	ConfigKey  string
 }
 
 func NewCommandFlags() *CommandFlags {
 	return &CommandFlags{
-		Conf:       "",
 		Env:        "",
-		ConfigHost: "",
+		ConfigPath: "",
 		ConfigType: "",
+		ConfigHost: "",
+		ConfigKey:  "",
 	}
 }
 
 func (f *CommandFlags) Init() {
-	flag.StringVar(&f.Conf, "conf", "./config.yaml", "config path, eg: -conf bootstrap.yaml")
+	flag.StringVar(&f.ConfigPath, "conf", "./config.yaml", "config path, eg: -conf bootstrap.yaml")
 	flag.StringVar(&f.Env, "env", "dev", "runtime environment, eg: -env dev")
-	flag.StringVar(&f.ConfigHost, "chost", "127.0.0.1:8500", "config server host, eg: -chost 127.0.0.1:8500")
 	flag.StringVar(&f.ConfigType, "ctype", "consul", "config server host, eg: -ctype consul")
+	flag.StringVar(&f.ConfigHost, "chost", "127.0.0.1:8500", "config server host, eg: -chost 127.0.0.1:8500")
+	flag.StringVar(&f.ConfigKey, "ckey", "/config", "config key path, eg: -ckey /config")
 }
